e2a427826305f27951758bf2aee85ec4f3f486f0
rjaramilloh <159493247+rjaramilloh@users.noreply.github.com>
2024-03-16 21:51:02 -0500

    momento 3

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -19,6 +19,30 @@ public class Zona {
 	}
 	
 	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Zoologico getZoo() {
+		return zoo;
+	}
+
+	public void setZoo(Zoologico zoo) {
+		this.zoo = zoo;
+	}
+
+	public ArrayList<Animal> getAnimales() {
+		return animales;
+	}
+
+	public void setAnimales(ArrayList<Animal> animales) {
+		this.animales = animales;
+	}
+
 	public Zona () {
 		
 	}
--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -18,6 +18,30 @@ public class Zoologico {
 	}
 	
 	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getUbicacion() {
+		return ubicacion;
+	}
+
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+
+	public ArrayList<Zona> getZonas() {
+		return zonas;
+	}
+
+	public void setZonas(ArrayList<Zona> zonas) {
+		this.zonas = zonas;
+	}
+
 	public Zoologico () {
 		
 	}
